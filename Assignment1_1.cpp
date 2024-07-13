e10480dcb6187e859a57563510ce0ea59bbe3550
AgnimitraBanerjee <149256594+AgnimitraBanerjee@users.noreply.github.com>
2024-07-13 19:49:01 +0530

    Add files via upload

---
+++ Assignment1_1.cpp
@@ -0,0 +1,8 @@
+//Assignment1_Q1
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Hello, World!";
+	return 0;
+}
---
+++ Assignment1_2.cpp
@@ -0,0 +1,12 @@
+//Assignment1_Q2
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cout<<"Enter an integer number: ";
+	cin>>num;
+	int doubleNum = 2 * num;
+	cout<<"Double of "<<num<<" = "<<doubleNum;
+	return 0;
+}
